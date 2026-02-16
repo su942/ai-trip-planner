a3ce41a565915db821e1b91976a90e3b639da439
su942 <surajalam3004@gmail.com>
2026-02-16 20:25:40 +0530

    make logo,menu and button

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,13 +1,42 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'lucide-react'
+import Image  from 'next/image'
 import React from 'react'
+
+const menuOptions =[
+  {
+    name: 'Home',
+    path: '/'
+  },
+  {
+    name:'Pricing',
+    path:'/pricing'
+  },
+  {
+    name:'Contact us',
+    path: '/contact-us'
+  }
+]
 
 function Header() {
   return (
-    <div>
+    <div className='flex justify-between items-center p-4 '>
       {/*Logo*/}
-
+      <div className='flex gap-2 items-center'>
+          <Image src={'/logo.svg'} alt='logo' width={30} height={30}/>
+          <h2 className='font-bold text-2xl'> AI Trip Planner</h2>
+       </div>
       {/*Menu Options*/}
+      <div className='flex gap-8 items-center'>
+        {menuOptions.map((menu, index) =>(
+          <Link href={menu.path}>
+            <h2 className='text-lg hover:scale-105 transition-all hover:text-primary' >{menu.name}</h2>
+          </Link>
 
+        ))}
+      </div>
       {/*Get Started Button*/}
+      <Button>Get Started</Button>
     </div>
   )
 }
